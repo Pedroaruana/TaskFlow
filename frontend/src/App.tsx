8387aeb3dee476b6cb033933cc3de43d14dacf16
PEDRO <aruanapedro@gmail.com>
2026-08-01 18:48:16 -0300

    feat: add password recovery pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/login/signIn'
 import Register from './pages/register/signUp'
+import ForgotPassword from './pages/forgotPassword/forgotPassword'
+import VerifyOtp from './pages/verifyOtp/verifyOtp'
+import ResetPassword from './pages/resetPassword/resetPassword'
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/esqueci-minha-senha" element={<ForgotPassword />} />
+        <Route path="/verificar-otp" element={<VerifyOtp />} />
+        <Route path="/resetar-senha" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   )
