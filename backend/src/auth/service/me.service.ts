6be6a8ab6a6e985df63a71a4c28fd3ded29e5c84
aubert-co <blacket021@gmail.com>
2026-08-03 18:14:05 -0300

    refactor: simplify auth me service

--- backend/src/auth/service/me.service.ts
+++ backend/src/auth/service/me.service.ts
@@ -1,10 +1,8 @@
-import { formatUser, getUserByIdAuth } from "../../helpers/functions.helper"
-import { AppError } from "../../middlewares/app.error"
+import {  getUserByIdAuth } from "../../helpers/functions.helper"
+
 
 export const meService = async (id:number) => {
   const user = await getUserByIdAuth(id)
-  if(!user){
-    throw new AppError("Unauthorized",401)
-  }
+
  return user
 }
