628dd2625e8ff6b2623c6584d5e54eb035ffea7f
aubert-co <blacket021@gmail.com>
2026-08-03 18:03:37 -0300

    feat: add testing infrastructure

--- backend/src/middlewares/rate-limit.ts
+++ backend/src/middlewares/rate-limit.ts
@@ -1,8 +1,9 @@
 import rateLimit from "express-rate-limit";
 
+const isProduction = process.env.NODE_ENV
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: isProduction? 50 : 5,
   standardHeaders: true,
   legacyHeaders: false,
 });
