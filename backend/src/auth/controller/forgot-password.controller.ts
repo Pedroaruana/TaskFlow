2ad03affafbd43b1903302aed5df930b5f1944bd
aubert-co <blacket021@gmail.com>
2026-08-05 17:27:37 -0300

    refactor(backend): simplify forgot password logic

--- backend/src/auth/controller/forgot-password.controller.ts
+++ backend/src/auth/controller/forgot-password.controller.ts
@@ -1,5 +1,4 @@
 import { RequestHandler } from "express";
-import { AppError } from "../../middlewares/app.error";
 import { forgotPasswordSchema } from "../schema/forgot-password.schema";
 import { otpSchema } from "../schema/otp.schema";
 import { forgotPasswordService } from "../service/forgot-password.service";
@@ -9,10 +8,8 @@ import bcrypt from "bcrypt";
 export const forgotPasswordController: RequestHandler = async (req, res, next) => {
   try{
       const data = forgotPasswordSchema.parse(req.body);
-      const result = await forgotPasswordService(data.email);
-
-      if(!result) return new AppError("email not exists", 404);
-
+      await forgotPasswordService(data.email);
+    
       const helper = new NodemailerHelper("exemple@gmail.com", "passemailexample");
       const otp = helper.generateOtp(6);  
       const hashOtp = await bcrypt.hash(otp, 10);
@@ -29,8 +26,7 @@ export const forgotPasswordController: RequestHandler = async (req, res, next) =
          error: null,
          data: object_otp
       });
-  }
-  catch(error){
-      return console.log(error);
+  }catch(error){
+    next(error)
   }
 }
